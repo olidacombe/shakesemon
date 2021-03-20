b20ef21b7a64be850655643971bcdad67f42c405
Oli Dacombe <olidacombe@gmail.com>
2021-03-20 14:05:25 +0000

    get_pokemon_description_from_name stub with unit test

--- src/lib.rs
+++ src/lib.rs
@@ -24,3 +24,30 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
         .run();
     Ok(server)
 }
+
+async fn get_pokemon_description_from_name(name: &str) -> Result<String, std::io::Error> {
+    Ok(format!("description of {}", name))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[actix_rt::test]
+    async fn test_get_pokemon_description_from_name() {
+        let test_cases = vec![
+            ("Wormadam", "When the bulb on its back grows large, it appearsto lose the ability to stand on its hind legs."),
+        ];
+
+        for (name, description) in test_cases {
+            match get_pokemon_description_from_name(name).await {
+                Ok(fetched_description) => assert_eq!(
+                    fetched_description, description,
+                    "Expected description `{}` for {}, received `{}`",
+                    description, name, fetched_description
+                ),
+                Err(e) => assert!(false, "Error fetching description for {}: `{}`", name, e),
+            }
+        }
+    }
+}
