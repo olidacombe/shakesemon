c5aca94268462ae9254d8c7615cfbb8773a76e5d
Oli Dacombe <olidacombe@gmail.com>
2021-03-20 22:47:52 +0000

    some translation fetching functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use actix_web::dev::Server;
 use actix_web::{get, web, App, HttpServer, Responder};
 use pokerust::{FlavorText, FromName, PokemonSpecies};
+use reqwest;
 use serde::{Deserialize, Serialize};
 use std::net::TcpListener;
 
@@ -8,6 +9,38 @@ use std::net::TcpListener;
 pub struct Pokemon {
     pub name: String,
     pub description: String,
+}
+
+#[derive(Deserialize)]
+struct TranslationContents {
+    translated: String,
+}
+
+#[derive(Deserialize)]
+struct Translation {
+    contents: TranslationContents,
+}
+
+async fn get_shakespearean_translation(plain: &str) -> Result<String, String> {
+    let client = reqwest::Client::new();
+    match client
+        .post(&format!(
+            "{}",
+            "https://api.funtranslations.com/translate/shakespeare.json"
+        ))
+        .form(&[("text", plain)])
+        .send()
+        .await
+    {
+        Ok(response) => match response.json::<Translation>().await {
+            Ok(translation) => return Ok(translation.contents.translated),
+            _ => Err("Invalid translation received".to_owned()),
+        },
+        _ => Err(format!(
+            "Failed to fetch shakespearean translation for {}",
+            plain
+        )),
+    }
 }
 
 #[get("/pokemon/{name}")]
@@ -64,6 +97,24 @@ mod tests {
                     description, name, fetched_description
                 ),
                 Err(e) => assert!(false, "Error fetching description for {}: `{}`", name, e),
+            }
+        }
+    }
+
+    #[actix_rt::test]
+    async fn test_get_shakespearean_translation() {
+        let test_cases = vec![
+            ("You gave Mr. Tim a hearty meal, but unfortunately what he ate made him die.", "Thee did giveth mr. Tim a hearty meal,  but unfortunately what he did doth englut did maketh him kicketh the bucket."),
+        ];
+
+        for (plain, expected) in test_cases {
+            match get_shakespearean_translation(plain).await {
+                Ok(received) => assert_eq!(
+                    expected, received,
+                    "Expected translation `{}` for `{}`, received `{}`",
+                    expected, plain, received
+                ),
+                Err(e) => assert!(false, "Error fetching translation for {}: `{}`", plain, e),
             }
         }
     }
