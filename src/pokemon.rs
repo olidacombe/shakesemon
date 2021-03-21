5ac0d40e8db12300e6f1ca7689cf616ab422f669
Oli Dacombe <olidacombe@gmail.com>
2021-03-21 20:59:36 +0000

    move unit tests to modules

--- src/pokemon.rs
+++ src/pokemon.rs
@@ -22,3 +22,26 @@ pub fn get_pokemon_description_from_name(name: &str) -> Result<String, Error> {
         _ => Err(Error::NoPokemonDescription),
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_pokemon_description_from_name() {
+        let test_cases = vec![
+            ("wormadam", "When BURMY evolved, its cloak\nbecame a part of this Pokémon’s\nbody. The cloak is never shed."),
+        ];
+
+        for (name, description) in test_cases {
+            match get_pokemon_description_from_name(name) {
+                Ok(fetched_description) => assert_eq!(
+                    fetched_description, description,
+                    "Expected description `{}` for {}, received `{}`",
+                    description, name, fetched_description
+                ),
+                Err(e) => assert!(false, "Error fetching description for {}: `{}`", name, e),
+            }
+        }
+    }
+}
--- src/translation.rs
+++ src/translation.rs
@@ -21,3 +21,26 @@ pub async fn get_shakespearean_translation(plain: &str) -> Result<String, Error>
     let translation = response.json::<Translation>().await?;
     Ok(translation.contents.translated)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[actix_rt::test]
+    async fn test_get_shakespearean_translation() {
+        let test_cases = vec![
+            ("You gave Mr. Tim a hearty meal, but unfortunately what he ate made him die.", "Thee did giveth mr. Tim a hearty meal,  but unfortunately what he did doth englut did maketh him kicketh the bucket."),
+        ];
+
+        for (plain, expected) in test_cases {
+            match get_shakespearean_translation(plain).await {
+                Ok(received) => assert_eq!(
+                    expected, received,
+                    "Expected translation `{}` for `{}`, received `{}`",
+                    expected, plain, received
+                ),
+                Err(e) => assert!(false, "Error fetching translation for {}: `{}`", plain, e),
+            }
+        }
+    }
+}
