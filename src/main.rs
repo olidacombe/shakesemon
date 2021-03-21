9bb1d5bf3f69c1e484524ab347f82a7176f282df
Oli Dacombe <olidacombe@gmail.com>
2021-03-21 14:53:42 +0000

    heed clippy's warnings

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ async fn main() -> std::io::Result<()> {
     let bind_address: String =
         std::env::var("BIND_ADDRESS").unwrap_or_else(|_| "127.0.0.1:0".to_owned());
     // Bubble up the io::Error if we failed to bind the address
-    let listener =
-        TcpListener::bind(&bind_address).expect(&format!("Failed to bind {}", &bind_address));
+    let listener = TcpListener::bind(&bind_address)
+        .unwrap_or_else(|_| panic!("Failed to bind {}", &bind_address));
     run(listener)?.await
 }
